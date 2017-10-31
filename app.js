577e395a3dff74279cf13088491fddbec5f82803
binbobo <2249441872@qq.com>
2017-10-31 16:06:15 +0800

    添加compression以及server-favicon中间件

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var http = require('http');
 var express = require('express');
 var app = express();
+var path = require('path');
+var favicon = require('serve-favicon');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('port', process.env.PORT || 3000);
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+
+app.set('port', process.env.PORT || 80);
 
 app.all('*', function (req, res) {
-  res.sendFile('index.html');
+  res.sendFile('index.html', {root: path.join(__dirname, 'public')});
 });
 
 http.createServer(app).listen(app.get('port'), function () {
