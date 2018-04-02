3ea9400d4be6796d6554473a14b705906861e823
wangbin <wangbin@missfresh.cn>
2018-04-02 11:41:19 +0800

    注册时检查用户是否已存在逻辑

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -6,24 +6,30 @@ const DBCommonHander = require('../utils/db')
 var UserModel = mongoose.model('User', UserSchema);
 
 const login = function (req, res) {
-  UserModel.findOne({'username': req.body.username}, { password: 0}, function (err, user) {
+  UserModel.findOne({'username': req.body.username}, function (err, user) {
     if (err) return DBCommonHander.errorHandler(err, res, '检查用户是否存在失败');
-    console.log(user)
     if (user == null) return DBCommonHander.errorHandler(err, res, '用户不存在');
     if (user.password !== req.body.password) return DBCommonHander.errorHandler(err, res, '密码错误');
-    return DBCommonHander.successHandler( res, '登陆成功', user);
+    // 设置Cookie 标识当前登陆用户
+    return DBCommonHander.successHandler( res, '登陆成功', {
+      name: user.username,
+      email: user.email
+    });
   });
 };
 
 const register = function (req, res) {
   const {username, password} = req.body
   if (!username || !password) return DBCommonHander.errorHandler(err, res, '用户名或者密码不能为空');
-  // 创建一个新的blog
-  const user = new UserModel(req.body);
-  user.save(function (err, user) {
-    if (err) return DBCommonHander.errorHandler(err, res, '注册失败');
-    return DBCommonHander.successHandler( res, '注册成功', null);
-  });
+  UserModel.findOne({'username': req.body.username}, function (err, user) {
+    if (err) return DBCommonHander.errorHandler(err, res, '检查用户是否存在失败');
+    if (user != null) return DBCommonHander.errorHandler(err, res, '用户已存在');
+    // 创建一个新的用户
+    new UserModel(req.body).save(function (err, user) {
+      if (err) return DBCommonHander.errorHandler(err, res, '注册失败');
+        return DBCommonHander.successHandler( res, '注册成功', null);
+      });
+    });
 };
 
 module.exports = {
